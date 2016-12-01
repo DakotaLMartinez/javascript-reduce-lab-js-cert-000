003b4f6ec8ea8f4e89767a003a68cbb78a157df3
Dakota Martinez <dakotaleemusic@gmail.com>
2016-12-01 00:11:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries = batteryBatches.reduce( (total, batchCount) => {
+  return total + batchCount;
+});
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +20,20 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+const wordCountMap = monologueLines.reduce( (map, line) => {
+  let lineWordCount = String(countWords(line));
+  if (lineWordCount in map) {
+    map[lineWordCount]++;
+  } else {
+    map[lineWordCount] = 1;
+  }
+  return map;
+}, {});
+
+function countWords(line) {
+  let output = line.replace(/(^\s*)|(\s*$)/gi,"");
+  output = output.replace(/[ ]{2,}/gi," ");
+  output = output.replace(/\n /,"\n");
+  return output.split(' ').length;
+}
